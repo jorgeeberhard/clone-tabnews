6a47d47b9f29d84b8bf982882c896416e854919f
Eberhard <90481642+jorgeeberhard@users.noreply.github.com>
2024-12-06 21:19:10 +0000

    refatoração do `migrations.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,12 +3,16 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  if (request.method !== "GET" && request.method !== "POST") {
-    response.status(405).end();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
   }
 
-  const dbClient = await database.getNewClient();
+  let dbClient;
   try {
+    dbClient = await database.getNewClient();
     const defaultMigrationsOptions = {
       dbClient: dbClient,
       dryRun: true,
@@ -20,7 +24,7 @@ export default async function migrations(request, response) {
 
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-      response.status(200).json(pendingMigrations);
+      return response.status(200).json(pendingMigrations);
     }
 
     if (request.method === "POST") {
@@ -30,10 +34,9 @@ export default async function migrations(request, response) {
       });
 
       if (migratedMigrations.length > 0) {
-        response.status(201).json(migratedMigrations);
-        return;
+        return response.status(201).json(migratedMigrations);
       }
-      response.status(200).json(migratedMigrations);
+      return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
     console.error(error);
