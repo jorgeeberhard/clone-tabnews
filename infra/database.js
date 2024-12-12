b42485c554fdd8b3f3e637074f798e5564a4ef28
Eberhard <90481642+jorgeeberhard@users.noreply.github.com>
2024-12-12 00:59:36 +0000

    add `lint:eslint:check` script with `ESLint`

--- infra/database.js
+++ infra/database.js
@@ -15,24 +15,17 @@ async function query(queryObject) {
 }
 
 async function getNewClient() {
-  let client;
-  try {
-    client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      ssl: getSSLValues(),
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
 
-    await client.connect();
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    return client;
-  }
+  await client.connect();
+  return client;
 }
 
 export default {
