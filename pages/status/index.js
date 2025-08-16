02a2402b88973cb4c3df2307ea574a27e4ff169f
Eberhard <90481642+jorgeeberhard@users.noreply.github.com>
2025-08-16 21:38:58 +0000

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,47 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+    </>
+  )
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000
+  })
+
+  let updatedAtText = "Carregando..."
+  let maxConnectionsText = "Carregando...";
+  let versionText = "Carregando...";
+  let openedConnectionsText = "Carregando..."
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    maxConnectionsText = data.dependencies.database.max_connections;
+    versionText = data.dependencies.database.version;
+    openedConnectionsText = data.dependencies.database.opened_connections;
+  }
+
+  return (
+    <div>
+      <p>Última atualização: {updatedAtText}</p>
+      <div>
+        <h2>Database</h2>
+        <p>Versão: {versionText}</p>
+        <p>Conexões abertas: {openedConnectionsText}</p>
+        <p>Conexões máximas: {maxConnectionsText}</p>
+      </div>
+    </div>
+  )
+
+}
